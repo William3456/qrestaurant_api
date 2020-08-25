f4d0f7c0ab4696949a5584f4597ed59b89bd4755
William Lazo <williamvasquez962@gmail.com>
2020-08-25 11:01:48 -0600

    Creando BD

---
+++ database/migrations/2014_10_12_162538_create_restaurante_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestauranteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurante', function (Blueprint $table) {
+            $table->bigIncrements('id_restaurante');
+            $table->string("nombre",255);
+            $table->unsignedBigInteger('num_mesas');
+            $table->unsignedBigInteger('id_usuario');
+            $table->string("telefono",9);
+            $table->string("direccion",255);
+            $table->string("correo",255)->unique();
+            $table->timestamps();
+            $table->foreign('id_usuario')->references('id_usuario')->on('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurante');
+    }
+}
